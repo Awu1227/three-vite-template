ac5e495c61bd63d33d3c266f57fc27cf81c05e44
Orange <72426886+Awu1227@users.noreply.github.com>
2023-08-19 10:54:54 +0800

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+
+
+export default defineConfig({
+  plugins:[
+  ],
+  base: '/',
+  build: {
+    cssCodeSplit: true // 是否提取所有CSS到一个CSS文件中, introduct: https://cn.vitejs.dev/config/build-options.html#build-csscodesplit
+  },
+})
